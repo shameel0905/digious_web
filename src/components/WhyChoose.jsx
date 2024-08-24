0cb4060f62400a28a8b863ec7d1504630d7d440e
devolution007 <waleedqureshi285@gmail.com>
2024-08-25 00:58:23 +0500

    updated about page

--- src/components/WhyChoose.jsx
+++ src/components/WhyChoose.jsx
@@ -9,10 +9,10 @@ const WhyChoose = () => {
             <div data-aos="fade-up" data-aos-delay="500" className="title-area text-center me-xl-4 ms-xl-4">
               <span className="sub-title">
                 <img src="assets/img/icon/title_left.svg" alt="shape" />
-                Career Opportunities
+                Digious Solutions: 
               </span>
               <h2 className="sec-title style2">
-                Your Business Goals with the Confidence
+              Innovative IT and Marketing Excellence Worldwide
               </h2>
             </div>
           </div>
@@ -23,7 +23,7 @@ const WhyChoose = () => {
           <div className="col-xl-6">
             <div className="wcu-thumb">
               <div className="wcu-thumb-1">
-                <img src="assets/img/normal/wcu-thumb-1-1.png" alt="img" />
+                <img src="assets/img/normal/worldwide.png" alt="img" />
               </div>
               <div className="wcu-shape jump">
                 <img src="assets/img/normal/1.png" alt="img" />
@@ -41,10 +41,10 @@ const WhyChoose = () => {
                 </div>
                 <div className="wcu-card-details">
                   <h4 className="wcu-card-title">
-                    Networ Infra Tructure Solutions
+                  Global Services
                   </h4>
                   <p className="wcu-card-text">
-                    IT Technology is a category a encompassing{" "}
+                  Offering IT and marketing solutions worldwide.{" "}
                   </p>
                 </div>
               </div>
@@ -54,10 +54,10 @@ const WhyChoose = () => {
                 </div>
                 <div className="wcu-card-details">
                   <h4 className="wcu-card-title">
-                    IT Training and most a the Education
+                  Skilled Team
                   </h4>
                   <p className="wcu-card-text">
-                    IT Technology is a category a encompassing{" "}
+                  Expert designers, developers, and marketing specialists.{" "}
                   </p>
                 </div>
               </div>
@@ -67,10 +67,10 @@ const WhyChoose = () => {
                 </div>
                 <div className="wcu-card-details">
                   <h4 className="wcu-card-title">
-                    Technology solutions for a brighter
+                  Modern Techniques
                   </h4>
                   <p className="wcu-card-text">
-                    IT Technology is a category a encompassing
+                  Utilizing updated codes and tech trends.
                   </p>
                 </div>
               </div>
@@ -79,9 +79,9 @@ const WhyChoose = () => {
                   <img src="assets/img/icon/wcu-icon_2-4.svg" alt="img" />
                 </div>
                 <div className="wcu-card-details">
-                  <h4 className="wcu-card-title">IT Projec Manage Services</h4>
+                  <h4 className="wcu-card-title">Customer Satisfaction </h4>
                   <p className="wcu-card-text">
-                    IT Technology is a category a encompassing
+                  Ensuring client fulfilment as our priority.
                   </p>
                 </div>
               </div>
