08f72379b31a80cf6cb39841bebd928973159afb
Devolution <devolution@digioussolutions.com>
2025-04-16 04:10:36 +0500

    feat: add image-webpack-loader and configure webpack for image optimization
    - Added image-webpack-loader to devDependencies for image optimization.
    - Updated webpack.config.js to include rules for processing images (gif, png, jpeg, svg) with file-loader and image-webpack-loader.
    - Enabled Babel loader for JavaScript and added support for SCSS/CSS files.
    - Removed commented-out script for disabling Dev Tools in index.html.
    - Created a backup of the original webpack.config.js.

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -3,5 +3,44 @@ module.exports = {
     output: {
       publicPath: '/',
     },
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+            },
+          },
+          {
+            test: /\.(sa|sc|c)ss$/,
+            use: ["style-loader", "css-loader", "sass-loader"],
+          },
+          {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [
+              "file-loader",
+              {
+                loader: "image-webpack-loader",
+                options: {
+                  mozjpeg: {
+                    progressive: true,
+                  },
+                  optipng: {
+                    enabled: false,
+                  },
+                  pngquant: {
+                    quality: [0.65, 0.9],
+                    speed: 4,
+                  },
+                  gifsicle: {
+                    interlaced: false,
+                  },
+                },
+              },
+            ],
+          },
+        ],
+    },
     // other configurations...
-  };
+};
